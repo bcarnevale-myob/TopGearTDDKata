55cbbd986e8701646463852f0ab9360cf31e7730
bcarnevale-myob <bianca.carnevale@myob.com>
2019-04-15 15:52:09 +1000

    Added edge case tests on gears

--- java/src/test/java/com/lagerweij/GearBoxTest.java
+++ java/src/test/java/com/lagerweij/GearBoxTest.java
@@ -3,20 +3,13 @@ package com.lagerweij;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class GearBoxTest {
 
-    @Test
-    public void testSomething() {
-        GearBox gearBox = new GearBox();
-        gearBox.doit(7);
-
-        assertTrue(true);
-    }
+    public static final int NEUTRAL_GEAR = 0;
 
     @Test
-    public void previousRPMIsNextRPMAfterDoIt() {
+    public void doItUpdatesPreviousRPM() {
         GearBox gearBox = new GearBox();
         gearBox.doit(1232);
         int firstRPM = gearBox.getPreviousRPM();
@@ -28,12 +21,31 @@ public class GearBoxTest {
     }
 
     @Test
+    public void gearboxStartsAtZeroRPM() {
+        GearBox gearBox = new GearBox();
+        int actualGear = gearBox.getGearNumber();
+
+        assertEquals(NEUTRAL_GEAR, actualGear);
+
+    }
+
+    @Test
     public void changesToFirstGearAt500RPM() {
         GearBox gearBox = new GearBox();
         gearBox.doit(500);
-        int gearNumber = gearBox.getGearNumber();
+        int actualGear = gearBox.getGearNumber();
 
-        assertEquals(1, gearNumber);
+        assertEquals(1, actualGear);
+    }
+
+    @Test
+    public void doesNotChangeToSecondGearAt2000RPM(){
+        GearBox gearBox = new GearBox();
+        gearBox.doit(500);
+        gearBox.doit(2000);
+        int actualGear = gearBox.getGearNumber();
+
+        assertEquals(1, actualGear);
     }
 
     @Test
@@ -41,9 +53,20 @@ public class GearBoxTest {
         GearBox gearBox = new GearBox();
         gearBox.doit(500);
         gearBox.doit(2001);
-        int gearNumber = gearBox.getGearNumber();
+        int actualGear = gearBox.getGearNumber();
 
-        assertEquals(2, gearNumber);
+        assertEquals(2, actualGear);
+    }
+
+    @Test
+    public void doesNotChangeDownAt500RPM(){
+        GearBox gearBox = new GearBox();
+        gearBox.doit(500);
+        gearBox.doit(2001);
+        gearBox.doit(500);
+        int actualGear = gearBox.getGearNumber();
+
+        assertEquals(2, actualGear);
     }
 
     @Test
@@ -52,10 +75,65 @@ public class GearBoxTest {
         gearBox.doit(500);
         gearBox.doit(2001);
         gearBox.doit(499);
-        int gearNumber = gearBox.getGearNumber();
+        int actualGear = gearBox.getGearNumber();
 
-        assertEquals(1, gearNumber);
+        assertEquals(1, actualGear);
     }
 
+    @Test
+    public void inFirstGearAndGivenZeroRPMStaysInFirst() {
+        GearBox gearBox = new GearBox();
+        gearBox.doit(500);
+        gearBox.doit(0);
+        int actualGear = gearBox.getGearNumber();
 
+        assertEquals(1, actualGear);
+    }
+
+    @Test
+    public void hasSixGears() {
+        GearBox gearBox = new GearBox();
+        gearBox.doit(500);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        int actualGear = gearBox.getGearNumber();
+        
+        assertEquals(6, actualGear);
+    }
+    @Test
+    public void changesDownOneGearAtATime() {
+        GearBox gearBox = new GearBox();
+        gearBox.doit(500);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(499);
+        gearBox.doit(499);
+        int actualGear = gearBox.getGearNumber();
+
+        assertEquals(4, actualGear);
+    }
+
+    @Test
+    public void sixthGearIsTopGear() {
+        GearBox gearBox = new GearBox();
+        gearBox.doit(500);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        gearBox.doit(2001);
+        int actualGear = gearBox.getGearNumber();
+
+        assertEquals(6, actualGear);
+    }
 }
